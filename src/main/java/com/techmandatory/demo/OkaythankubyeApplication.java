4997a4aca3aa503b15f4ddcc9115178a4acad41b
yewonblairseo@gmail.com <yewonblairseo@gmail.com>
2019-12-09 18:30:32 +0100

    initial setup

---
+++ src/main/java/com/techmandatory/demo/OkaythankubyeApplication.java
@@ -0,0 +1,13 @@
+package com.techmandatory.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkaythankubyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OkaythankubyeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techmandatory/demo/OkaythankubyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techmandatory.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkaythankubyeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
